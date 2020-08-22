0d51f00bce214a0080030245be93803cb0e12731
Dapo Adeleke <adelekeoladapo@gmail.com>
2020-08-22 11:07:36 +0100

    Added Swagger for API documentation

--- src/main/java/com/bankwithmint/verifycard/VerifyCardApplication.java
+++ src/main/java/com/bankwithmint/verifycard/VerifyCardApplication.java
@@ -7,7 +7,16 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
+@EnableSwagger2
 @SpringBootApplication
 public class VerifyCardApplication {
 
@@ -30,6 +39,29 @@ public class VerifyCardApplication {
 		config.addAllowedMethod("*");
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
+	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/card-scheme/**"))
+				.apis(RequestHandlerSelectors.basePackage("com.bankwithmint.verifycard"))
+				.build()
+				.apiInfo(this.apiDetails());
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Verify Card Service",
+				"API for Card Verification",
+				"1.0",
+				"",
+				new springfox.documentation.service.Contact("Dapo Adeleke", "https://dapoadeleke.me", "adelekeoldadapo@gmail.com"),
+				"API License",
+				"https://dapoadeleke.me",
+				Collections.emptyList()
+		);
 	}
 
 }
